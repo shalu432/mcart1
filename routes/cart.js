2cfa819a5051bf9320e3f4a1c97ef1bdcf36c792
shalu shukla <shalushukla0799@gmail.com>
2022-04-06 11:40:55 +0530

    commit

--- routes/cart.js
+++ routes/cart.js
@@ -8,5 +8,5 @@ const cart = require('../controller/cartcontroller')
 router.post('/cart',cart.addProductToCart)
 router.put('/deletecart',middCustomer.JWT,cart.deleteCart)
 router.get('/getcart',middCustomer.JWT,cart.getAllCart)
-router.get('/getcart',middCustomer.JWT,cart.getCart)
+router.get('/getcart/:id',middCustomer.JWT,cart.getCart)
 module.exports = router
